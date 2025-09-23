10f8d768a4a367ee1f23ae1f1089d034bad3a37d
Joao Mesquita <jpmesquita29085@hotmail.com>
2025-09-24 00:53:38 +0100

    parsing complete

--- philo/parsing.c
+++ philo/parsing.c
@@ -6,7 +6,7 @@
 /*   By: jpmesquita <jpmesquita@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 22:48:14 by jpmesquita        #+#    #+#             */
-/*   Updated: 2025/09/24 00:19:52 by jpmesquita       ###   ########.fr       */
+/*   Updated: 2025/09/24 00:52:55 by jpmesquita       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,10 @@ static int is_digit(char **av)
 		j = 0;
 		while(av[i][j])
 		{
-			if (av[i][j] == '+')
+			if (av[i][j] == '+' || av[i][j] == '-')
 				j++;
 			else if (!(av[i][j] >= '0' && av[i][j] <= '9'))
 				return(0);
-			if (ft_atoi(av[i]) <= 0 && i != 5)
-				return (0);
 			j++;
 		}
 		i++;
@@ -36,13 +34,41 @@ static int is_digit(char **av)
 	return (1);
 }
 
+
 static int	philos_max_number(char **av)
 {
 	int	max_number;
 	
 	max_number = ft_atoi(av[1]);
 	if (max_number > 200 || max_number < 1)
-		return (0);
+	return (0);
+	return (1);
+}
+
+int	time_values(char **av)
+{
+	int	i;
+
+	i = 1;
+	while (av[i])
+	{
+		if (ft_atoi(av[i]) <= 0 && i != 5)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static int number_of_meals(int ac, char **av) 
+{
+	int meals_amount; 
+	
+	if(ac == 6) 
+	{
+		meals_amount = ft_atoi(av[5]); 
+		if (meals_amount < 0) 
+			return (0); 
+	}
 	return (1);
 }
 
@@ -61,6 +87,16 @@ void	check_args(int ac, char **av)
 	else if (!philos_max_number(av))
 	{
 		write(2, "Philosophers must be 1-200\n", 27);
+		exit(1);
+	}
+	else if (!time_values(av))
+	{
+		write(2, "Time values must be greater than 0\n", 35);
+		exit(1);
+	}
+	else if (!number_of_meals(ac, av))
+	{
+		write(2, "SHIT!\n", 6);
 		exit(1);
 	}
 }
